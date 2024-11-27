94e074c158310b95db454cbe495fae215d8a9f9f
sensei <manishkumar97430@gmail.com>
2024-11-27 21:53:13 +0530

    updated routes list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,15 +76,18 @@ const tools = [
     description: "Convert SVG to PNG",
     link: "/svg",
   },
+  {
+    name: "Shapes Generator",
+    description: "Beautiful Shapes Generator",
+    link: "/shapes",
+  },
 ];
 
 export default function Home() {
   return (
     <div className="space-y-12">
       <section className="py-12 md:py-24 lg:py-32 xl:py-48">
-        <div
-         className="container px-4 md:px-6 max-w-7xl mx-auto"
-         >
+        <div className="container px-4 md:px-6 max-w-7xl mx-auto">
           <div className="flex flex-col items-center space-y-4 text-center">
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
